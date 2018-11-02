de4914093d6d1b04e75dc1cf9a226e6867b85666
vrszabi <vr.szabi@gmail.com>
2018-11-02 21:46:43 +0100

    Update feladat_1.c

--- feladat_1.c
+++ feladat_1.c
@@ -5,11 +5,6 @@
 #include <string.h>
 int main()
 {
-    //printf("Az e betuk szama a szovegben: %i", count_e(".\eszamlal.txt"));
-    //masol("d:/games/dvdrum.rar", "d:/games/proba/dvdrum_masolt.rar");
-    //printf("Ennyi kis a betu van a fileban: %d",olvas("./text2.txt"));
-//    printf("sikeresen beirtam 8 valamit, talán %d",ir_lowlevel("./beir.txt"));
-
 int fileaz=-2;
 int fileaz_magan=-2;
 int fileaz_massal=-2;
@@ -25,28 +20,32 @@ fileaz_massal=open("adat3.txt",O_CREAT|O_TRUNC|O_RDWR,S_IREAD|S_IWRITE);
 
 
 fileaz=open("adat1.txt", O_RDONLY); //megnyitjuk olvasásra a filet
-while(read(fileaz,&c,sizeof(c))){
-    if(c=='a' || c=='e' || c=='u' || c=='i' || c=='o' || c=='A' || c=='E' || c=='O' || c=='I' || c=='U'){
-        write(fileaz_magan,&c,1); //beírjuk az adat2.txt-be
+while(read(fileaz,&c,sizeof(c))){ //ez a blokk nagyon fontos, itt nézzük meg hogy magánhangzo vagy mássalhangzó a karakter
+    if(c=='a' || c=='e' || c=='u' || c=='i' || c=='o' || c=='A' || c=='E' || c=='O' || c=='I' || c=='U'){ //itt megnézzük hogy magánhangzó-e
+        write(fileaz_magan,&c,1); //ha igen beírjuk az adat2.txt-be
         beirt_maganhanzo++;
-    }else if(isalpha(c)){ //ha nem magánhangzó, DE betű, akkor lépünk ebbe az ágba
+    }else if(isalpha(c)){ //ha nem magánhangzó, DE betű, akkor csak mássalhangzó lehet.
         write(fileaz_massal,&c,1); //beírjuk az adat3.txt-be
         beirt_massalhangzo++;
     }
 }
-printf("\n\nA beirt maganhangzok szama: %d",beirt_maganhanzo);
-char mgh=beirt_maganhanzo+'0';
+
+char mgh=beirt_maganhanzo+'0'; //itt megkapjuk karakterként a beirt karakterek számát
 char msh=beirt_massalhangzo+'0';
 
-write(fileaz_magan,&mgh,1);
+write(fileaz_magan,&mgh,1); //és beírjuk a fájl végére
 write(fileaz_massal,&msh,1);
+//////////////////////////////////////////
 // 2 fontos dolog amit nem tanitottak:
-//isalpha() fuggvény, megnézi hogy betű-e az átadott karakter
+//isalpha() fuggvény, megnézi hogy betű-e az átadott karakter.
+
 //integer+'0' -> igy megkapod az integert char-ként, pl 5+'0' egyenlő lesz 5-tel mint karakter
 //több számjegyű számnál nemtudom hogy működik, nem próbáltam
+////////////////////////////////////////////
 close(fileaz);
 close(fileaz_magan);
 close(fileaz_massal);
+
 
 int fileaz_adat2=-2;
 int fileaz_adat3=-2;
@@ -54,16 +53,16 @@ int fileaz_adat3=-2;
 fileaz_adat2=open("adat2.txt",O_RDONLY); //CSAK OLVASÁSRA NYITJUK MEG
 fileaz_adat3=open("adat3.txt",O_RDONLY);
 
-printf("\n\nAz adat2.txt tartalma: ");
-while(read(fileaz_adat2,&c,sizeof(c))){
-    printf("%c",c);
+printf("Az adat2.txt tartalma: ");
+while(read(fileaz_adat2,&c,sizeof(c))){ //amíg tud olvasni a fájlból, vagyis
+    printf("%c",c); //nem 0 a visszatérítési érték, addig kiirja karakterenként a tartalmát
 }
-printf("\n\n");
 
 printf("\nAz adat3.txt tartalma: ");
 while(read(fileaz_adat3,&c,sizeof(c))){
     printf("%c",c);
 }
+
 printf("\n");
 
 return 0;
