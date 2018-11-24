80d8bac3220e175be44a3a7e248423171880bd39
vrszabi <vr.szabi@gmail.com>
2018-11-24 20:58:38 +0100

    Create define_declare.c

---
+++ define_declare.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/////////// 1.  Definiálunk
+fuggveny_1(argumentum1, argumentum2);
+fuggveny_2(argumentum1, argumentum2);
+fuggveny_3(argumentum1, argumentum2);
+
+int main()
+{
+/////////// 3.  Fő függvényben meghívjuk függvényeinket
+fuggveny_1 meghivása
+fuggveny_2 meghivása
+fuggveny_3 meghivása
+
+return 0;
+}
+
+//////////// 2.  A fő függvény lezárása után deklaráljuk, vagyis megírjuk a függvényeket
+fuggveny_1(argumentum1, argumentum2)
+{
+  fuggveny_1 torzse
+}
+
+fuggveny_2(argumentum1, argumentum2)
+{
+  fuggveny_2 torzse
+}
+
+fuggveny_3(argumentum1, argumentum2)
+{
+  fuggveny_3 torzse
+}
